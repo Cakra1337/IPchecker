1e422c2f73e66a3d3a20fd4d7bb4fc047bf279b9
Cakra1337 <1337CyberCl0wn@gmail.com>
2017-07-12 18:59:43 +0700

    Update ipchecker.py

--- ipchecker.py
+++ ipchecker.py
@@ -1,20 +1,22 @@
 import ipaddress
 import socket
 
-print('----- Python IP Checker -----')
+print '----- Python IP Checker -----'
 def main():
-	print('1.Check My Local IP')
-	print('2.Coming soon.')
-	choose = input('Choose : ')
-	if(choose == '1'):
-		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		localIP = s.getsockname()[0]
-		print('----local IP : ',localIP)
-		print('----IP Version : ',ipaddress.ip_address(localIP).version)
-		s.close()
+        print '1.Check My Local IP'
+        print '2.Coming soon.'
+        choose = raw_input('Choose : ')
+        if choose == '1':
+                s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                s.connect_ex(('8.8.8.8',80))
+                localIP = s.getsockname()[0]
+                ip = ipaddress.ip_address(unicode(localIP))
+                print '----local IP : ',localIP
+                print '----IP Version : ',ip.version
+                s.close()
 
-	else:
-		print('Coming soon.')
-		main()
+        elif choose == '2' :
+                print 'Coming soon.'
+                main()
 
 main()
